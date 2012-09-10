1875c5f1a813abda0ecf52b5b2604011fe2f2c15
Edwin Amsler <edwinamsler@thinkboxsoftware.com>
2012-09-10 14:29:02 -0500

    Add a sample Pre/Post job

---
+++ Examples/Scripting/PostJob/Sample/Sample.py
@@ -0,0 +1,22 @@
+#Python.NET
+###############################################################
+# This is an Python.net/CPython script.                       #
+# To use IronPython, remove  "#Python.NET" from the first     #
+# line of this file. Make sure you don't use the quotes.      #
+###############################################################
+
+###############################################################
+## Imports
+###############################################################
+from System.IO import *
+from Deadline.Scripting import *
+import os
+
+###############################################################
+## Entry point and other source
+###############################################################
+def __main__():
+	""" This is run by Deadline before or after a task depending on which context its used in """
+
+	LogInfo("Script ran.")
+	LogInfo("...And did absolutely nothing")
