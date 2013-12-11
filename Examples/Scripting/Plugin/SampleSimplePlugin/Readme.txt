a4e855874d7f0ed4e2920d14ec5980fd3bfd3b5a
Edwin Amsler <edwinamsler@thinkboxsoftware.com>
2013-12-11 14:45:56 -0600

    Updated Plugin example for 6.x

--- Examples/Scripting/Plugin/SampleSimplePlugin/Readme.txt
+++ Examples/Scripting/Plugin/SampleSimplePlugin/Readme.txt
@@ -1,4 +1,5 @@
-Behold! A sample regular plugin! Or, sample simple plugin. For Deadline 5.x only
+Behold! A sample regular plugin! Or, sample simple plugin. Now Deadline 6.x
+only!
 
 You might say that this script is similar to the events plugins. You'd
 be right. Why mess with a good thing right? So, the big question is what's
@@ -7,19 +8,18 @@ the difference?
 Mainly, plugins are the heart of the render process in Deadline. They handle
 setting up the environment, getting the arguments just right based on submitted
 info, and figure out which render executable to use. Look in other plugins
-for great examples. I stole most of this from the TileAssembler!
+for great examples. I stole most of this from the Deadline 5.1 TileAssembler!
 
-To start hacking on this, copy the whole thing into the Repository's "plugins"
-folder, then rename files/folders. Basically, do a find/replace for the files
-and folders and contents named "SampleSimplePlugin" to what you want it to be
-called. Be sure to rename everyting to match.
+To start hacking on this, copy the whole thing into the "[repo]\custom\plugins"
+folder, then rename files/folders. Basically, rename any files or folders that
+are named "SampleSimplePlugin" to what you want it to be referenced by in
+Deadline. Be sure to rename everyting to match. Like socks.
 
 The really tricky part for this is submitting a job so that this plugin is
 called! I don't have an example of that yet, but you'll need to look into how
 the submission process works for now. One day I'll get an example submitter
 here too. For now, hyperlink!:  (who calls them that anymore? I'm clearly an OG)
 http://www.thinkboxsoftware.com/deadline-5-scriptpluginsdk/
-
 
 If you have problems, send an e-mail to support@thinkboxsoftware.com
 or go over to the forums. Lots of helpful folks over there.
--- Examples/Scripting/Plugin/SampleSimplePlugin/SampleSimplePlugin.py
+++ Examples/Scripting/Plugin/SampleSimplePlugin/SampleSimplePlugin.py
@@ -17,82 +17,138 @@ from Deadline.Scripting import *
 
 ######################################################################
 ## This is the function that Deadline calls to get an instance of the
-## overloaded plugin class.
+## plugin. It allows us to abstract away the name
 ######################################################################
 def GetDeadlinePlugin():
-	return SampleSimplePlugin()
-	
-######################################################################
-## This is the object we pass back to Deadline. Not all functions
-## are represented yet. I'm still learning this stuff myself. :D
-##                                                - Edwin
-######################################################################
+    return SampleSimplePlugin()
+    
 class SampleSimplePlugin(DeadlinePlugin):
-	MyClassScopedVariable = 0
-	
-	## Called by Deadline to initialize the process.
-	def InitializeProcess( self ):
-		self.SingleFramesOnly = True
-		self.PluginType = PluginType.Simple
-		
-		self.UseProcessTree = True
-		self.StdoutHandling = True
-		
-		self.AddStdoutHandler( "ERROR:.*", self.HandleError )
-		self.AddStdoutHandler( "Fake progress ([0-9]*) of ([0-9]*)", self.HandleProgress )
-	
-	def PreRenderTasks( self ):
-		Environment.SetEnvironmentVariable("Cake", "Flake")
-		LogInfo("Preparing for awesome sauce.")
-	
-	def RenderExecutable( self ):
-		LogInfo("Supplying program executable for sauce collection.")
-		return "cmd.exe"
-	
-	def RenderArgument( self ):
-		LogInfo("Argumentating (supplying command line arguments)")
+    MyClassScopedVariable = 0
+    
+    def __init__(self):
+        """
+            Used to set up really basic stuff like event callbacks. In general,
+            we use the same function names across the board for these, so using
+            this whole function verbatim is likely a good idea.
+        """
+        
+        self.InitializeProcessCallback += self.InitializeProcess
+        self.RenderExecutableCallback += self.RenderExecutable
+        self.RenderArgumentCallback += self.RenderArgument
+        self.PreRenderTasksCallback += self.PreRenderTasks
+        self.PostRenderTasksCallback += self.PostRenderTasks
+    
+    def Cleanup(self):
+        """
+            With Python.net we need to clean up after ourselves or risk leaking
+            memory in the interpreter. Any callbacks assigned *MUST* be removed
+        """
+        
+        for stdoutHandler in self.StdoutHandlers:
+            del stdoutHandler.HandleCallback
+        
+        del self.InitializeProcessCallback
+        del self.RenderExecutableCallback
+        del self.RenderArgumentCallback
+        del self.PreRenderTasksCallback
+        del self.PostRenderTasksCallback
+    
+    def InitializeProcess( self ):
+        """
+            Here we initialize the plugin more deeply. I think this needs
+            to be here instead of the init function since the plugin should
+            be fully loaded at this point
+        """
+        
+        self.SingleFramesOnly = True # Does this plugin support ranges?
+        self.PluginType = PluginType.Simple # We aren't to managing a process
+        
+        self.UseProcessTree = True # End the process we spawn if the slave exits
+        self.StdoutHandling = True # Actually watch the output.
+        
+        self.AddStdoutHandlerCallback("ERROR:.*").HandleCallback += self.HandleError
+        self.AddStdoutHandlerCallback("Fake progress ([0-9]*) of ([0-9]*)").HandleCallback += self.HandleProgress
+    
+    def PreRenderTasks( self ):
+        """
+            Stuff to do before we start the render process
+        """
+        
+        Environment.SetEnvironmentVariable("Cake", "Flake")
+        self.LogInfo("Preparing for awesome sauce.")
+    
+    def PostRenderTasks( self ):
+        """
+            Stuff to do after the render process has exited.
+            Note that this and the PreRender work can also be
+            done via an external script so you can plug in
+            different functionality based on the submitted job
+        """
+        
+        self.LogInfo("Sauce may or may not have been acquired.")    
+    
+    def RenderExecutable( self ):
+        """
+            Return the executable path to run here. In all of our plugins
+            we allow specifying the path to the executable in the "configure
+            plugins" section of the Monitor, and usually we verify what
+            version we'll be running. That version number is saved during
+            submission, but it's not actually a feature built into the core
+            of Deadline. Fun facts :)
+        """
+        
+        self.LogInfo("Supplying program executable for sauce collection.")
+        return "cmd.exe"
+    
+    def RenderArgument( self ):
+        """
+            Return the argument list we'll pass to the program to run.
+            This is usually where the magic happens of converting submission
+            option boxes into command line flags. The exceptions of course
+            being the complex plugins like MayaBatch and 3DSMax plugins.
+        """
+    
+        self.LogInfo("Argumentating (supplying command line arguments)")
 
-		# Some example calls to configuration params in the dlinit/params files
-		config1 = GetConfigEntry("SampleProperty")
-		config2 = int(GetConfigEntryWithDefault("FakeProp1", -1))
-		
-		# These values are submitted with the plugin specific job file.
-		option  = GetPluginInfoEntry("option")
-		
-		# Lots more handy functions here:
-		# http://www.thinkboxsoftware.com/deadline-5-scriptpluginsdk/
-		
-		return "/c echo Sauce please!"
-	
-	def PostRenderTasks( self ):
-		LogInfo("Sauce may or may not have been acquired.")	
-	
-	def HandleError( self ):
-		"""
-			This is actually user-specified. You'll notice it from the
-			AddStdoutHandler calls above. We just like calling it this.
-		"""
-		LogInfo("Sauce related problems occured!")
-		
-		# All of the calls from AddStdoutHandler() will populate the
-		# regex matches for the call it invokes. So, if you want to grab a
-		# specific tile, frame, error condition from the output line that
-		# matched, use brackets for capture groups. It'll always be returned
-		# as text, so cast/convert accordingly!
-		# Also, FailRender() does exactly what you expect. It throws a special
-		# exception that blows up the RenderTask function.
-		FailRender( self.GetRegexMatch(0) )
-	
-	def HandleProgress( self ):
-		"""
-			This is one is user-specified too. It can be named anything, though
-			it's our practice to name it this.
-		"""
-		
-		# Specifying a float out of 100 will let the Slave update the per-task
-		# progress. That value doesn't count directly to the job progress yet,
-		# but it's handy to see where people's tasks are at.
-		SetProgress(59.6)
+        # Some example calls to configuration params in the dlinit/params files
+        config1 = self.GetConfigEntry("SampleProperty")
+        config2 = int(self.GetConfigEntryWithDefault("FakeProp1", "-1"))
+        
+        # These values are submitted with the plugin specific job file.
+        option  = self.GetPluginInfoEntryWithDefault("option", "")
+        
+        # Lots more handy functions here:
+        # http://www.thinkboxsoftware.com/deadline-5-scriptpluginsdk/
+        
+        return "/c echo Sauce please!"
+    
+    def HandleError( self ):
+        """
+            This is actually user-specified. You'll notice it from the
+            AddStdoutHandler calls above. We just like calling it this.
+        """
+        
+        self.LogInfo("Sauce related problems occured!")
+        
+        # All of the calls from AddStdoutHandler() will populate the
+        # regex matches for the call it invokes. So, if you want to grab a
+        # specific tile, frame, error condition from the output line that
+        # matched, use brackets for capture groups. It'll always be returned
+        # as text, so cast/convert accordingly!
+        # Also, FailRender() does exactly what you expect. It throws a special
+        # exception that blows up the RenderTask function.
+        FailRender( self.GetRegexMatch(0) )
+    
+    def HandleProgress( self ):
+        """
+            This is one is user-specified too. It can be named anything, though
+            it's our practice to name it this.
+        """
+        
+        # Specifying a float out of 100 will let the Slave update the per-task
+        # progress. That value doesn't count directly to the job progress yet,
+        # but it's handy to see where people's tasks are at.
+        SetProgress(59.6)
 
-		# See the "HandleError" function for how to pull progress-like things
-		# from the match that is done.
+        # See the "HandleError" function for how to pull progress-like things
+        # from the match that is done.
