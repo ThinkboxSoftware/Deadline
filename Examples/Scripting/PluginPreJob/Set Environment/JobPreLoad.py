f616ce3bdb15b79305b7e68aec3ee1f3e7e55830
Edwin Amsler <edwinamsler@thinkboxsoftware.com>
2014-10-30 14:46:49 -0500

    Changed script pydoc string

--- Examples/Scripting/PluginPreJob/Set Environment/JobPreLoad.py
+++ Examples/Scripting/PluginPreJob/Set Environment/JobPreLoad.py
@@ -1,6 +1,15 @@
-# Set environment variables for current plugin
-# Should work with 6.0 and up, maybe 5.x as well.
-# Written by Edwin Amsler <support@thinkboxsoftware.com>
+'''
+    JobPreLoad.py - Set environment variables for current plugin
+    
+    This should work on Deadline 6 and 7's APIs. If there are problems,
+    please report them to support@thinkboxsoftware.com
+    
+    RepositoryUtils.GetSlaveSettings will return an empty, valid,
+    SlaveSettings object. Change 'slaveNameX' entries below to avoid
+    creating fake Slaves within the Database.
+    
+    Written by Edwin Amsler <support@thinkboxsoftware.com>
+'''
 
 ###############################################################
 ## Imports
