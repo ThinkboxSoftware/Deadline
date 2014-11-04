37fbd4c37a051ba32e8b288b737fbd2970cf1958
Edwin Amsler <edwinamsler@thinkboxsoftware.com>
2014-11-04 12:19:52 -0600

    Add a new Slave state example

---
+++ Examples/Scripting/DeadlineCommand/IsSlaveRunning/GetSlaveState.py
@@ -0,0 +1,28 @@
+'''
+    GetSlaveState.py - List if Slaves are running or not
+    
+    This should work on Deadline 6 and 7's APIs. If there are problems,
+    please report them to support@thinkboxsoftware.com
+'''
+
+from Deadline.Scripting import *
+
+stateMapping = {
+    # This maps different states to what power management sees
+    "Idle":         "On",
+    "Rendering":    "On",
+    "Starting Job": "On",    
+    "Offline":      "Off",
+    "Stalled":      "Off",
+    # This state is due to a missing slaveInfo document and
+    # shouldn't be acted upon
+    "Unknown":      "Unknown"
+}
+
+def __main__():
+    slaveInfos = RepositoryUtils.GetSlaveInfos(True)
+
+    for slaveInfo in slaveInfos:
+        state = stateMapping[slaveInfo.SlaveState]
+    
+        print("{0: <30}{1}".format(slaveInfo.SlaveName, state))
---
+++ Examples/Scripting/DeadlineCommand/IsSlaveRunning/Readme.txt
@@ -0,0 +1,12 @@
+This is an example of how you can pull the Slave's current state as the power
+management code would consider it. It's mostly helpful for showing how to pull
+Slave information from the Deadline API, and what possible running states the
+Slave might have.
+
+To run this script, just copy it somewhere and call it using DeadlineCommand's
+ExecuteScript command like so:
+
+C:\Program Files\Thinkbox\Deadline6\bin\DeadlineCommand ExecuteScript GetSlaveState.py
+
+As usual, if you have questions, problems, or comments, send them over to
+support@thinkboxsoftware.com. Feel free to blame Edwin in the subject
