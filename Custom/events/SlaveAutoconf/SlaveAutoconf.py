4ea7a414d64afd2b8583bca5b4893f4c12579e36
Edwin Amsler <edwinamsler@thinkboxsoftware.com>
2015-05-28 15:07:24 -0500

    Added automatic configuration event example
    Originally written by Gavin over here:

---
+++ Custom/events/SlaveAutoconf/SlaveAutoconf.py
@@ -0,0 +1,55 @@
+'''
+    Stolen from Gavin's great example on this forum thread:
+    https://forums.thinkboxsoftware.com/viewtopic.php?f=11&t=13396#p59978
+'''
+
+from System.Diagnostics import *
+from System.IO import *
+from System import TimeSpan
+
+from Deadline.Events import *
+from Deadline.Scripting import *
+
+import re
+import sys
+import os
+import subprocess
+import traceback
+import shlex
+
+SLAVE_NAME_PREFIX = "mobile-"
+POOLS = ["one", "two", "three"]
+
+
+def GetDeadlineEventListener():
+    return ConfigSlaveEventListener()
+
+
+def CleanupDeadlineEventListener(eventListener):
+    eventListener.Cleanup()
+
+
+class ConfigSlaveEventListener (DeadlineEventListener):
+    def __init__(self):
+        self.OnSlaveStartedCallback += self.OnSlaveStarted
+
+    def Cleanup(self):
+        del self.OnSlaveStartedCallback
+
+    # This is called every time the Slave starts
+    def OnSlaveStarted(self, slavename):
+        if not slavename.lower().startswith(SLAVE_NAME_PREFIX):
+            return
+
+        print("Slave automatic configuration for {0}".format(slavename))
+        for pool in POOLS:
+            try:
+                print("\tAdding pool {0}".format(pool))
+                RepositoryUtils.AddPoolToSlave(slavename, pool)
+
+                # Power management example:
+                # pmanage = RepositoryUtils.GetPowerManagementOptions()
+                # pmanage.Groups[0].SlaveNames.append(slavename)
+
+            except:
+                ClientUtils.LogText(traceback.format_exc())
---
+++ Custom/events/SlaveAutoconf/SlaveAutoconf.rst
@@ -0,0 +1,6 @@
+SlaveAutoconf.py
+==================================
+
+This script written originally by Gavin Greenwalt and mangled by Edwin Amsler
+shows how to have new Slaves automatically configure a set of pools each time
+they start up.
