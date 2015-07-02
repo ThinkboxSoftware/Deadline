c6b72208e5df746f31532f9507790572336ea1fb
Edwin Amsler <edwinamsler@thinkboxsoftware.com>
2015-07-02 14:01:17 -0500

    Add Zoë's script for splitting out task logs

---
+++ Tools/Logs/log-splitter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+'''
+	Take a Deadline Slave log and put the individual task output into
+	distinct files. Currently does not support concurrent tasks in the
+	log.
+'''
+
+import re
+import argparse
+
+parser = argparse.ArgumentParser(description='Extract task logs from Slave log')
+parser.add_argument('file', help='Slave log to extract from')
+args = parser.parse_args()
+
+original = open(args.file, "r")
+
+new = open("slave.txt", "w")
+job = 0
+jobs = open("task%s.txt" %job, "w")
+
+for line in original:
+	m = re.search('^.{20}\s*(\d{1,2}):', line)
+	end = re.search('Exited ThreadMain', line)
+
+	if end: 
+		jobs.write(line)
+		job += 1 
+		jobs.close()
+		jobs = open("jobs%s.txt" %job, "w")
+	if m: 
+		jobs.write(line)
+	else: 
+		new.write(line)
+
+jobs.close()
+new.close()
+original.close()
