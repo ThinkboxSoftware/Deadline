c9ac46aaf73d4aed32c2f7435b8121c3e3b0a687
Zoë Rose <zoe.rose@thinkboxsoftware.com>
2015-07-06 15:03:27 -0500

    handling multiple tasks

--- Tools/Logs/log-splitter.py
+++ Tools/Logs/log-splitter.py
@@ -17,22 +17,43 @@ original = open(args.file, "r")
 
 new = open("slave.txt", "w")
 job = 0
-jobs = open("task%s.txt" % job, "w")
+jobs = None
+
+files = {"0":None, 
+         "1":None, 
+         "2":None, 
+         "3":None, 
+         "4":None, 
+         "5":None, 
+         "6":None, 
+         "7":None, 
+         "8":None, 
+         "9":None, 
+         "10":None, 
+         "11":None, 
+         "12":None, 
+         "13":None, 
+         "14":None, 
+         "15":None, }
 
 for line in original:
-    m = re.search('^.{20}\s*(\d{1,2}):', line)
-    end = re.search('Exited ThreadMain', line)
-
-    if end:
-        jobs.write(line)
-        job += 1
-        jobs.close()
-        jobs = open("task%s.txt" % job, "w")
+    m = re.search('^.{20}\s+(\d{1,2}):', line)    
     if m:
-        jobs.write(line)
+        position = line[21:24].strip()
+        position = position.replace(":", "")
+
+        if files[position] == None:
+            files[position] = open("task%s.txt" % position, "w")
+            jobs = files[position]
+        else:
+            jobs = files[position]
+            jobs.write(line)
     else:
         new.write(line)
 
-jobs.close()
+# Cleanup
+for key, value in files.iteritems():
+    if value is not None:
+        value.close()
 new.close()
 original.close()
