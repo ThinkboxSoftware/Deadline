950c61e4ec3dfbb947883716c32a3653c7074390
Justin Blagden <justin.blagden@thinkboxsoftware.com>
2016-02-19 12:13:22 -0600

    Created loop to step through OS envrionment Checks against user-created list of unwanted variables

--- Custom/events/CustomEnvironmentCopy/CustomEnvironmentCopy.py
+++ Custom/events/CustomEnvironmentCopy/CustomEnvironmentCopy.py
@@ -10,7 +10,7 @@ from Deadline.Scripting import *
 # This is the function called by Deadline to get an instance of the Draft event listener.
 #########################################################################################
 def GetDeadlineEventListener():
-    return OverrideJobNameListener()
+    return CustomEnvironmentCopyListener()
 
 def CleanupDeadlineEventListener(eventListener):
     eventListener.Cleanup()
@@ -18,7 +18,7 @@ def CleanupDeadlineEventListener(eventListener):
 ###############################################################
 # The event listener class.
 ###############################################################
-class OverrideJobNameListener (DeadlineEventListener):
+class CustomEnvironmentCopyListener (DeadlineEventListener):
     def __init__(self):
         self.OnJobSubmittedCallback += self.OnJobSubmitted
     
@@ -26,9 +26,24 @@ class OverrideJobNameListener (DeadlineEventListener):
         del self.OnJobSubmittedCallback
 
     def OnJobSubmitted(self, job):
-       
+       	#List of environment keys you don't want passed.
+       	unwantedkeys = self.GetConfigEntryWithDefault("UnwantedEnvKeys", "")
+
+        #Split out on commas, gettting rid of excess whitespace
+        unwantedkeys = [x.strip() for x in unwantedkeys.split(',')]
+
+       	wantedkeys = []
+
         self.LogInfo("On Job Submitted Event Plugin: Custom Environment Copy Started")
 
-        
+        #Go through the current system environment variables not copying the unwanted keys
+        for key in os.environ:
+        	if key not in unwantedkeys:
+        		wantedkeys.append(key)
+
+        #Set chosen variables to job
+        for key in wantedkeys:
+        	self.LogInfo("Setting %s to %s" % (key, os.environ[key][:50])) #only print out the first x characters of an environment value
+    		job.SetProcessEnvironmentVariable(key, os.environ[key])
 
         self.LogInfo("On Job Submitted Event Plugin: Custom Environment Copy Finished")
