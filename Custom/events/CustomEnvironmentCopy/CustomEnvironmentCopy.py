8b8adec0a4c9feb517af3479e056f8b0f9ce2c14
Justin Blagden <justin.blagden@thinkboxsoftware.com>
2016-02-19 15:02:30 -0600

    Added additional logging, and very crude case insensitive comparisons. Changed the call to change environment variables to a job-only one. Remembered to save the job

--- Custom/events/CustomEnvironmentCopy/CustomEnvironmentCopy.py
+++ Custom/events/CustomEnvironmentCopy/CustomEnvironmentCopy.py
@@ -6,6 +6,8 @@ from System import *
 from Deadline.Events import *
 from Deadline.Scripting import *
 
+import os
+import string
 #########################################################################################
 # This is the function called by Deadline to get an instance of the Draft event listener.
 #########################################################################################
@@ -26,24 +28,29 @@ class CustomEnvironmentCopyListener (DeadlineEventListener):
         del self.OnJobSubmittedCallback
 
     def OnJobSubmitted(self, job):
-       	#List of environment keys you don't want passed.
-       	unwantedkeys = self.GetConfigEntryWithDefault("UnwantedEnvKeys", "")
+        #List of environment keys you don't want passed.
+        unwantedkeys = self.GetConfigEntryWithDefault("UnwantedEnvKeys", "")
 
-        #Split out on commas, gettting rid of excess whitespace
-        unwantedkeys = [x.strip() for x in unwantedkeys.split(',')]
+        #Split out on commas, gettting rid of excess whitespace and make uppercase
+        unwantedkeys = [string.upper(x.strip()) for x in unwantedkeys.split(',')]
 
-       	wantedkeys = []
+        wantedkeys = []
+
+        self.LogInfo("Unwanted Environment keys defined as:")
+        self.LogInfo(str(unwantedkeys))
 
         self.LogInfo("On Job Submitted Event Plugin: Custom Environment Copy Started")
 
         #Go through the current system environment variables not copying the unwanted keys
         for key in os.environ:
-        	if key not in unwantedkeys:
-        		wantedkeys.append(key)
+            if string.upper(key) not in unwantedkeys:
+                wantedkeys.append(key)
 
         #Set chosen variables to job
         for key in wantedkeys:
-        	self.LogInfo("Setting %s to %s" % (key, os.environ[key][:50])) #only print out the first x characters of an environment value
-    		job.SetProcessEnvironmentVariable(key, os.environ[key])
+            self.LogInfo("Setting %s to %s" % (key, os.environ[key]))
+            job.SetJobEnvironmentKeyValue(key, os.environ[key])
+
+        RepositoryUtils.SaveJob(job)
 
         self.LogInfo("On Job Submitted Event Plugin: Custom Environment Copy Finished")
