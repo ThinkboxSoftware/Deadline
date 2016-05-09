885ecad99994cc133c96b75d2439b246c1266402
Justin Blagden <justin.blagden@thinkboxsoftware.com>
2016-05-09 12:23:55 -0500

    Moved README into the script + fixed some pep stuff

--- Custom/scripts/Tasks/CreatePreviewJobFromTasks.py
+++ Custom/scripts/Tasks/CreatePreviewJobFromTasks.py
@@ -1,6 +1,12 @@
 # ---------------------------------------
 # Created by Justin Blagden - March 2016
+#
+# Drop this into your [repo]\custom\scripts\Tasks to install.
+# Select one or more tasks to create a preview job out of.
+# This will mark the selected tasks as completed on the main job.
+# Send all comments, complaints and inqueries to support@thinkboxsoftware.com - be sure to mention Justin in the subject.
 # ---------------------------------------
+
 from Deadline.Scripting import *
 from Deadline.Jobs import *
 from Deadline.Users import *
