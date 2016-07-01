003a6425621c15606319ed3ecf6537f2b969cee2
James <jamescoulter@thinkboxsoftware.com>
2016-07-01 11:48:40 -0700

    Extended and revised the REST API examples for bash. (#23)

--- Examples/REST API/Shell/requeue_job.sh
+++ Examples/REST API/Shell/requeue_job.sh
@@ -1,4 +1,16 @@
-SERVER=http://192.168.2.186:8080
-JOB_ID=53ed30b6f4b70d991c6a3be0
+#!/usr/bin/env bash
 
-curl -i -H "Accept: application/json" -X PUT -d "{'Command': 'requeue', 'JobID': '$JOB_ID'}" $SERVER/api/jobs
+# Example of requeing a Job using REST via curl.
+#
+# See:  http://docs.thinkboxsoftware.com/products/deadline/8.0/1_User%20Manual/manual/rest-jobs.html
+#
+
+SERVER=http://localhost:8082
+JOB_ID=5773e4ea86e590000fa27110
+
+JSONDATA='{"Command":"requeue","JobID":"'$JOB_ID'"}'
+
+RESPONSE=$(curl -H "Accept: application/json" -X PUT -d "$JSONDATA" $SERVER/api/jobs)
+
+echo $RESPONSE
+
